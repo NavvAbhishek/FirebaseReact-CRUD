7df48774dfc2eb363e1224e4a0870182a2898723
NavvAbhishek <abi2000navi@gmail.com>
2024-11-18 14:22:15 +0530

    login with username pw

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Auth from "./components/auth";
+
+const App = () => {
+  return (
+    <div>
+      <Auth />
+      Hii
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/auth.jsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className="grid  items-center gap-4 max-w-md md:max-w-lg  w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
+        <div className="w-full px-4 py-4">
+          <form>
+            <div className="mb-12">
+              <h3 className="text-gray-800 text-3xl font-extrabold">Sign in</h3>
+            </div>
+
+            <div>
+              <label className="text-gray-800 text-xs block mb-2">Email</label>
+              <div className="relative flex items-center">
+                <input
+                  name="email"
+                  type="text"
+                  required
+                  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="mt-8">
+              <label className="text-gray-800 text-xs block mb-2">
+                Password
+              </label>
+              <div className="relative flex items-center">
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 px-2 py-3 outline-none"
+                  placeholder="Enter password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-wrap items-center justify-between gap-4 mt-6">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-3 block text-sm text-gray-800"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div>
+                <a
+                  href="javascript:void(0);"
+                  className="text-blue-600 font-semibold text-sm hover:underline"
+                >
+                  Forgot Password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-12">
+              <button
+                type="button"
+                className="w-full shadow-xl py-2.5 px-4 text-sm tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                onClick={signIn}
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
